8bbe26838c7d6eb0fc663d4703e25ad9d0ce28a0
Jeremy Hastings <jeremy.hastings@mac.com>
2019-12-21 12:27:45 -0800

    Created registration form for events.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,35 @@
 class RegistrationsController < ApplicationController
 
+  before_action :set_event
+
   def index
-    @event = Event.find(params[:event_id])
+    #@event = Event.find(params[:event_id])
     @registrations = @event.registrations
   end
 
+  def new
+    #@event = Event.find(params[:event_id])
+    @registration = @event.registrations.new
+  end
+
+  def create
+    #@event = Event.find(params[:event_id])
+
+    @registration = @event.registrations.new(registration_params)
+
+    if @registration.save
+      redirect_to event_registrations_url(@event), notice: "Thanks for registering!"
+    else
+      render :new
+    end
+  end
+
+  private
+  def registration_params
+    params.require(:registration).permit(:name, :email, :how_heard)
+  end
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
 end
