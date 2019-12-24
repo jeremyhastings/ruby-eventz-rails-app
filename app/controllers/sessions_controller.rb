3cbc3f1012d681381a5060301f61ba2b80e0c63c
Jeremy Hastings <jeremy.hastings@mac.com>
2019-12-24 07:12:27 -0800

    Updated create method in sessions controller to allow for sign in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    params[:email]
-    params[:password]
+    user = User.find_by(email: params[:email])
+    # Putting user checks to see if it exists prior to authenticating.  Short circuits to prevent error.
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email/password combination!"
+      render :new
+    end
   end
 
   def destroy
