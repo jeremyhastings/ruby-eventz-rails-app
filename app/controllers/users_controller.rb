96b76a2bfb3479b4d04f6ba4123c1e75fdbfe92f
Jeremy Hastings <jeremy.hastings@mac.com>
2019-12-25 11:19:23 -0800

    Moved current_user method to application Controller.
    Then made it into a helper method.  Also made a require_signin method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  before_action :require_signin, except: [:new, :create]
+
+  #def require_signin
+  #  unless current_user
+  #    redirect_to new_session_url, alert: "Please sign in first!"
+  #  end
+  #end
+
   def index
     @users = User.all
   end
